e8c0cc46d9eedec36f7df3ff66f60583d01c2847
piyachandak1210 <priyankachandak@nimapinfotech.com>
2022-03-26 15:31:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator  } from '@react-navigation/native-stack';
+//import Home from './screens/Home';
+//import Login from './screens/Login';
+import Home from './screens/Homesqlite';
+import Login from './screens/Loginsqlite';
+
+const Stack = createNativeStackNavigator ();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#0080ff'
+          },
+          headerTintColor: '#ffffff',
+          headerTitleStyle: {
+            fontSize: 25,
+            fontWeight: 'bold'
+          }
+        }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
